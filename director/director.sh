6d66e91c23c22ba46b172146df0f79b61fc2c469
root <root@holdmusic.us-central1-a.c.howemc.internal>
2020-05-20 08:21:58 +0000

    local changes

--- director/director.sh
+++ director/director.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # gunicorn3 --log-level debug --bind 127.0.0.1:5000 app:app
-gunicorn3 --bind 127.0.0.1:5000 app:app
+env GOOGLE_APPLICATION_CREDENTIALS=/home/holdmusic-proxy/HoldMusic/director/gcp-auth.json gunicorn --bind 127.0.0.1:5000 app:app
